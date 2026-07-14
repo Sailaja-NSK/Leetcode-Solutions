c801a19cbd015d235dbd24654b3fc916cb1e9ad5
Sailaja Nissankurao <sailajanissankurao@gmail.com>
2026-07-14 12:03:01 +0530

    Time: 0 ms (100%), Space: 42 MB (54.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int dp(int n , int[] dp_arr) {
-        if(n == 0 || n == 1) return 1;
-        if(dp_arr[n] != 0) return dp_arr[n];
-        return dp_arr[n] = dp(n-1,dp_arr) + dp(n-2,dp_arr);
-    }
-    public int climbStairs(int n) {
+     public int climbStairs(int n) {
         int[] dp_arr = new int[n+1];
-        int ans = dp(n,dp_arr);
 
-        return ans;
+        dp_arr[0] = 1;
+        dp_arr[1] = 1;
+        for(int i = 2;i <= n;i++) {
+            dp_arr[i] = dp_arr[i-1] + dp_arr[i-2];
+        }
+        return dp_arr[n];
     }
 }
