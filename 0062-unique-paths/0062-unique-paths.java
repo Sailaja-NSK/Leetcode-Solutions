70a47cc069c8ba988098c0de79a3ee5ea56cc974
Sailaja Nissankurao <sailajanissankurao@gmail.com>
2026-07-15 13:42:07 +0530

    Time: 1 ms (14.69%), Space: 41.9 MB (82.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,23 @@
 class Solution {
-    public int f(int i, int j,int[][] dp_arr) {
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
-        if(dp_arr[i][j] != -1) return dp_arr[i][j];
 
-        int up = f(i-1,j,dp_arr);
-        int left = f(i,j-1,dp_arr);
-
-        return dp_arr[i][j] = up+left;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp_arr = new int[m][n];
         for(int i = 0;i < m;i++){
             Arrays.fill(dp_arr[i],-1);
         }
-       
-        return f(m-1,n-1,dp_arr);
+
+        for(int i = 0;i < m;i++) {
+            for(int j = 0;j < n;j++) {
+                if(i == 0 && j == 0) dp_arr[i][j] = 1;
+                else {
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0) up = dp_arr[i-1][j];
+                    if(j > 0) left = dp_arr[i][j-1];
+                    dp_arr[i][j] = up + left;
+                }
+            }
+        }
+        return dp_arr[m-1][n-1];
     }
 }
