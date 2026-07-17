21fcc8ee05adf0028b02c7a03d04056133c8f65e
Sailaja Nissankurao <sailajanissankurao@gmail.com>
2026-07-17 11:07:07 +0530

    Time: 29 ms (87.5%), Space: 99.9 MB (68.75%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int m = Integer.MIN_VALUE;
+        int n = nums.length;
+        for(int i = 0;i < n;i++) {
+            m = Math.max(m,nums[i]);
+        }
+
+        long[] cnt = new long[m+1];
+        for(int i = 0;i < n;i++) {
+            cnt[nums[i]]++;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = i * 2; j <= m; j += i) {
+                cnt[i] += cnt[j];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            cnt[i] = (cnt[i] * (cnt[i] - 1)) / 2;
+        }
+
+        for (int i = m; i >= 1; i--) {
+            for (int j = i * 2; j <= m; j += i) {
+                cnt[i] -= cnt[j];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        int[] ans = new int[queries.length];
+        for (int k = 0; k < queries.length; k++) {
+            long q = queries[k] + 1;
+            int left = 1,
+                right = m;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (cnt[mid] >= q) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            ans[k] = left;
+        }
+
+        return ans;
+
+    }
+}
