00ad176bd61472a22ea0e88123b6651e7f32fee7
Sailaja Nissankurao <sailajanissankurao@gmail.com>
2026-07-22 20:58:43 +0530

    Time: 1 ms (99.72%), Space: 47.3 MB (38.58%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,22 +1,18 @@
 class Solution {
-    public int minJumps(int ind,int n,int[] nums,int[] dp){
-        if(ind >= n-1) return 0;
-        if(nums[ind] == 0) return Integer.MAX_VALUE;
-        int minStep = Integer.MAX_VALUE;
-        
-        if(dp[ind] != -1) return dp[ind];
-        for(int jump = 1;jump <= nums[ind];jump++) {
-            int subResult = minJumps(ind + jump,n,nums,dp);
-            if (subResult != Integer.MAX_VALUE) {
-                minStep = Math.min(minStep, 1 + subResult);
-            }
-        }
-        return dp[ind] = minStep;
-    }
     public int jump(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return  minJumps(0,n,nums,dp);
+        int jumps = 0;
+        int  l = 0,r = 0;
+        while(r < nums.length-1) {
+            int farthest = 0;
+            for(int i = l;i <= r;i++) {
+                farthest = Math.max(farthest,i+nums[i]);
+            }
+
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }
+
+        return jumps;
     }
 }
