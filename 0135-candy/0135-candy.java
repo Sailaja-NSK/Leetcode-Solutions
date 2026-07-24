3a1c919cc7530287246528e515f737c874763838
Sailaja Nissankurao <sailajanissankurao@gmail.com>
2026-07-24 15:39:41 +0530

    Time: 3 ms (86.57%), Space: 47.6 MB (51.4%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,31 +1,31 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] left = new int[n];
-
-        left[0] = 1;
-        for(int i = 1;i<n;i++) {
-            if(ratings[i]  > ratings[i-1]) {
-                left[i] = left[i-1] + 1;
-            } else {
-                left[i] = 1;
+        int sum = 1;
+        int i = 1;
+        while(i < n) {
+            if(ratings[i] == ratings[i-1]) {
+                sum = sum + 1;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i-1]) {
+                peak += 1;
+                sum = sum + peak;
+                i++;
+            }
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i-1]) {
+                sum = sum + down;
+                down += 1;
+                i++;
+            }
+            if(down > peak) {
+                sum = sum + (down-peak);
             }
         }
+        return sum;
 
-        int right = 1,curr = 1;
-        int ans = 0;
-        ans = Math.max(1,left[n-1]);
-        for(int i = n-2;i >= 0;i--) {
-            if(ratings[i] > ratings[i+1]) {
-                curr = right + 1;
-            } else {
-                curr = 1;
-            }
-            right = curr;
-            ans = ans + Math.max(left[i],curr);
-        }
-
-
-        return ans;
-    } 
+    }
 }
