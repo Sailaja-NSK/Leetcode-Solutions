c0c7176761c7c02e11df774ce75a8a6a07de6c19
Sailaja Nissankurao <sailajanissankurao@gmail.com>
2026-08-20 17:01:34 +0530

    Time: 1 ms (87.45%), Space: 45.5 MB (62.8%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,28 +1,30 @@
 class Solution {
-    public void generatePermutations(int index,int[] nums,int n,
-    List<List<Integer>> ans,List<Integer> temp,int[] mp) {
-
-        if(temp.size() == n) {
+    public void generatePermutations(int index,int[] nums,int n,List<List<Integer>> ans) {
+        if(index == n) {
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0;i < n;i++) {
+                temp.add(nums[i]);
+            }
             ans.add(new ArrayList<>(temp));
             return;
         }
 
-        for(int i = 0;i < n;i++) {
-            if(mp[i] != 1) {
-                mp[i] = 1;
-                temp.add(nums[i]);
-                generatePermutations(i+1,nums,n,ans,temp,mp);
-                temp.remove(temp.size()-1);
-                mp[i] = 0;
-            }
+        for(int i = index;i < n;i++) {
+            swap(index,i,nums);
+            generatePermutations(index+1,nums,n,ans);
+            swap(index,i,nums);
         }
+    }
+
+    public void swap(int index1,int index2,int[] nums) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-        int[] mp = new int[n];
-        Arrays.fill(mp,0);
-        generatePermutations(0,nums,n,ans,new ArrayList<>(),mp);
+        generatePermutations(0,nums,n,ans);
         return ans;
     }
 }
