d29e0ea4c93e4dbcec23738ef32fb6d926363018
Sailaja Nissankurao <sailajanissankurao@gmail.com>
2026-08-23 18:02:20 +0530

    Time: 2 ms (41.11%), Space: 47.2 MB (74.62%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,20 +1,51 @@
-class Solution {
-    public void dfs(int node,int[][] matrix,int[] visited,int n) {
-        visited[node] = 1;
-        for(int i = 0;i < n;i++) {
-            if(matrix[node][i] == 1 && visited[i] == 0){
-                dfs(i,matrix,visited,n);
-            }
+class DisJointSet{
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> rank = new ArrayList<>();
+    DisJointSet(int n) {
+        for(int i = 0;i <= n;i++) {
+            parent.add(i);
+            rank.add(0);
         }
     }
+
+    public int findParent(int node) {
+        if(parent.get(node) == node) {
+            return node;
+        }
+
+        int up = findParent(parent.get(node));
+        parent.set(node,up);
+        return parent.get(node);
+    }
+
+    public void union(int u,int v) {
+        int up_u = findParent(u);
+        int up_v = findParent(v);
+
+        if(up_u == up_v) return;
+
+        if(rank.get(up_u) < rank.get(up_v)) {
+            parent.set(up_u,up_v);
+        } else if(rank.get(up_u) > rank.get(up_v)) {
+            parent.set(up_v,up_u);
+        } else {
+            parent.set(up_v,up_u);
+            rank.set(up_u,rank.get(up_u)+1);
+        }
+    }
+}
+class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int count = 0;
-        int[] visited = new int[n];
+        DisJointSet ds = new DisJointSet(n);
+        int count = n;
+
         for(int i = 0;i < n;i++) {
-            if(visited[i] == 0 ){
-                count++;
-                dfs(i,isConnected,visited,n);
+            for(int j = i+1;j < n;j++) {
+                if(isConnected[i][j] == 1 && ds.findParent(i) != ds.findParent(j)) {
+                    count--;
+                    ds.union(i,j);
+                }
             }
         }
         return count;
